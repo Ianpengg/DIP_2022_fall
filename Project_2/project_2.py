d708c083ac9b6a00ce42644330ba84bc5f2907db
ianpengg <ian01050.ee10@nycu.edu.tw>
2022-11-02 15:54:45 +0800

    [fix] Modified the code

--- Project_2/project_2.py
+++ Project_2/project_2.py
@@ -16,10 +16,15 @@ def check_folder(path):
 
 def fft_2d(img, n_x, n_y):
     """
+
+    Parameters
+    ------------
     img: input image
     n_x: DFT dimension of x
     n_y: DFT dimension of y
 
+    Returns
+    -----------
     return 2d fft result shape: (n_x, n_y)
     """
     f = np.fft.fft2(img,(n_x, n_y))
@@ -29,8 +34,12 @@ def fft_2d(img, n_x, n_y):
 
 def ifft_2d(img):
     """
+    Parameters
+    ------------
     img: input image
     
+    Returns
+    ------------
     return 2d ifft result shape: (n_x, n_y)
     """
     fshift = np.fft.ifftshift(img)
@@ -40,6 +49,16 @@ def ifft_2d(img):
 
 
 def save_img(src, title, save_path):
+    """
+    save the image
+
+    Parameters
+    ---------
+    src: image 
+    title: string
+    save_path: where to save the image
+
+    """
     global SAVE
     plt.figure()
     plt.imshow(src, cmap="gray")
@@ -52,12 +71,19 @@ def save_img(src, title, save_path):
 
 def generate_GLPF(shape=(1200,1200), d_0=100):
     """
-    return 2-D Gaussian lowpass filter 
-    shape: (M, N)
+    generate 2-D Gaussian lowpass filter 
+    
 
-    params
-    shape: dimension of the gaussian lowpass filter
+    Parameters
+    ---------------
+    shape: dimension of the gaussian lowpass filter (M, N)
     d_0: cutoff frequency
+    
+
+    Returns
+    ---------------
+    Gaussian lowpass filter shape: (M, N) with cutoff frequency d_0
+    
     """
     m, n = shape
     x = np.linspace(0, m, num=m)
@@ -70,6 +96,21 @@ def generate_GLPF(shape=(1200,1200), d_0=100):
 
 
 def filtering(img, filter):
+    """
+    Filter the gray-scale image with the given filter
+
+    Parameters
+    -------
+    img: gray-scale image shape: (m x n) 
+    filter: filter shape: (2*m, 2*n)
+    
+
+    Returns
+    -------
+    result_list: list
+    which stores images during the process
+
+    """
     result_list = []
     m, n = img.shape
     x = np.arange(0, 2*n)
@@ -97,7 +138,17 @@ def filtering(img, filter):
     return result_list
    
 def plot_all_result(img_list, img_title, col=4, row=2):
+    """
+    plot all figures based on given img_list and img_title
 
+    Params
+    ---------- 
+    img_list: list of the images, and shape of each object in list is (h, w, 3).  
+    img_title: list of the image titles
+    
+    
+    
+    """
     fig, ax = plt.subplots(row, col, figsize=(16, 16))
     if row == 1:
         for j in range(col):
@@ -117,6 +168,15 @@ def plot_all_result(img_list, img_title, col=4, row=2):
     plt.show()   
 
 def find_highest_k_freq(img, k):
+    """
+    find the (u, v) of highest k frequency in the half-left region of the image
+
+    Params
+    -------
+    img: DFT result shape:(M, N)
+    k: number of how many highest (u, v) pairs would return
+    
+    """
     m, n = img.shape
 
     left_half = np.abs(img[:m, :n//2])
@@ -144,19 +204,23 @@ if __name__ == "__main__":
     img_save_path = os.getcwd()
     print(img_save_path)
     
+
+    # Load images 
     img_fruit = cv2.imread('fruit.tif', 0)
     img_kid = cv2.imread('kid.tif', 0)
     
+    # Do the 2D-DFT of the images with fft 
     fruit_fft = fft_2d(img_fruit, 600, 600)
     kid_fft = fft_2d(img_kid, 600, 600)
     
     fruit_magnitude_spectrum = 20 * np.log(np.abs(fruit_fft))
     kid_magnitude_spectrum = 20 * np.log(np.abs(kid_fft))
 
-    
+    # Generate the Gaussian lowpass and highpass filter 
     glpf = generate_GLPF(shape=(1200,1200), d_0=200)
     ghpf = 1 - glpf
 
+    # filter the image with the given filter
     kid_lpf = filtering(img_kid, glpf)
     kid_hpf = filtering(img_kid, ghpf)
     fruit_lpf = filtering(img_fruit, glpf)
@@ -165,24 +229,16 @@ if __name__ == "__main__":
     kid_lpf.insert(0,img_kid)
     title_list = ["1", "2", "3", "4", "5","6"]
 
+    #plot all result 
     plot_all_result(kid_lpf, title_list, col=4, row=2)
 
+    # save the (u, v) coordinates of the highest 25 frequencies in the half left region of (b)
     fruit_csv = find_highest_k_freq(fruit_fft, 25)
     kid_csv = find_highest_k_freq(kid_magnitude_spectrum, 25)
     save_csv(fruit_csv, "fruit_")
     save_csv(kid_csv, "kid_")
-    
-    
 
-    # region = fruit_fft[0:600, 0:300]
-    # indices =  np.argpartition(region.flatten(), -2)[-25:]
-    # print(np.vstack(np.unravel_index(indices, region.shape)).T)
-
-    
-    # indices =  np.argpartition(a.flatten(), -2)[-5:]
-    # print(np.vstack(np.unravel_index(indices, a.shape)).T)
-
-
+    # save the result images 
     save_img(fruit_magnitude_spectrum, "Fourier magnitude spectra (in Log scale)", "img_fruit/fruit_fourier_magnitude_spectrum")
     save_img(kid_magnitude_spectrum, "Fourier magnitude spectra (in Log scale)", "img_kid/kid_fourier_magnitude_spectrum")
     save_img(fruit_lpf[-1], "Gaussian lowpass filter result", "img_fruit/fruit_glpf")
@@ -190,6 +246,7 @@ if __name__ == "__main__":
     save_img(fruit_hpf[-1], "Gaussian highpass filter result", "img_fruit/fruit_ghpf")
     save_img(kid_hpf[-1], "Gaussian highpass filter result", "img_kid/kid_ghpf")
 
+    # whether to show the saved images
     # plt.show()
 
     
